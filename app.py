8df89d779c93eb217d87a58dec41633b0d7fa904
Carlos Salazar <carlosiusalazar@gmail.com>
2020-10-04 15:45:59 +0900

    Youtube tutorial 13

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, redirect, url_for, render_template, request, g, Response, session, flash
+from password_strength import PasswordPolicy
+from password_strength import PasswordStats
+
+app = Flask(__name__)
+
+policy = PasswordPolicy.from_names(
+    length=8,  # min length: 8
+    uppercase=1,  # need min. 2 uppercase letters
+    numbers=1,  # need min. 2 digits
+    strength=0.66 # need a password that scores at least 0.5 with its entropy bits
+)
+
+app.config['SECRET_KEY'] = '@#$%^&*('
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        password = request.form.get('password')
+        email = request.form.get('email')
+        stats = PasswordStats(password)
+        checkpolicy = policy.test(password)
+        if stats.strength() < 0.66:
+            print(stats.strength())
+            flash("Password not strong enough. Avoid consecutive characters and easily guessed words.")
+            return render_template('form.html')
+        else:
+            return render_template('success.html')
+
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
