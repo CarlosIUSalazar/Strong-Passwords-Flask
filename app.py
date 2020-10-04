7794988e439e78794ba802acee48c0721339092b
Carlos Salazar <carlosiusalazar@gmail.com>
2020-10-04 17:33:46 +0900

    app.py

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from password_strength import PasswordPolicy
 from password_strength import PasswordStats
 
 app = Flask(__name__)
-
 policy = PasswordPolicy.from_names(
     length=8,  # min length: 8
     uppercase=1,  # need min. 2 uppercase letters
     numbers=1,  # need min. 2 digits
     strength=0.66 # need a password that scores at least 0.5 with its entropy bits
 )
-
 app.config['SECRET_KEY'] = '@#$%^&*('
 
 @app.route('/', methods=['GET', 'POST'])
@@ -27,7 +25,6 @@ def index():
         else:
             print(stats.strength())
             return render_template('success.html')
-
     return render_template('form.html')
 
 if __name__ == '__main__':
